4360f0066b7bcd0d9c8340556466504a98beb5ce
Mate Kancsal <minecraft.bencmet@gmail.com>
2021-03-23 14:01:10 +0100

    Projekt fájlok

--- ora.py
+++ ora.py
@@ -5,3 +5,28 @@ from j_sampleclock import *
 
 class Digitalclock:
 
+    scr = Screen()
+    clk = Clock(scr)
+
+    secturtle = Turtle(shape="turtle")
+    minturtle = Turtle(shape="turtle")
+    hourturtle = Turtle(shape="turtle")
+
+    def __init__(self):
+        self.secturtle._delay(0)
+        self.secturtle.speed(0)
+        self.minturtle._delay(0)
+        self.minturtle.speed(0)
+        self.hourturtle._delay(0)
+        self.hourturtle.speed(0)
+
+        self.clk.setOnSecondChangeListener(self.writeSec)
+        self.clk.setOnMinuteChangeListener(self.writeMin)
+        self.clk.setOnHourChangeListener(self.writeHour)
+
+        self.Sectest()
+
+        self.scr.mainloop()
+
+    def Sectest(self):
+

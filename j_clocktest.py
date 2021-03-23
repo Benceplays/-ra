921ff195efb7eef66b78813d669f55079a735d0e
Mate Kancsal <minecraft.bencmet@gmail.com>
2021-03-23 14:26:54 +0100

    Projekt fájlok

--- j_clocktest.py
+++ j_clocktest.py
@@ -1,3 +1,4 @@
 from j_sampleclock import SampleClock
+from DigitClock import SampleClock
 
 SampleClock()
--- ora.py
+++ ora.py
@@ -1,16 +1,44 @@
-from datetime import *
 from turtle import *
+from j_sampleclock import SampleClock
 from j_clock import *
-from j_sampleclock import *
 
-class Digitalclock:
+class Kattintgato:
 
     scr = Screen()
+    t = Turtle()
     clk = Clock(scr)
-
     secturtle = Turtle(shape="turtle")
     minturtle = Turtle(shape="turtle")
     hourturtle = Turtle(shape="turtle")
+    def writeSec(self):
+        self.printToConsole()
+        self.secturtle.clear()
+        self.secturtle.reset()
+        self.secturtle.forward(30)
+
+    def writeMin(self):
+        self.minturtle.clear()
+        self.minturtle.reset()
+        self.minturtle.penup()
+        self.minturtle.setheading(90)
+        self.minturtle.forward(180)
+        self.minturtle.right(90 + 3)
+        self.minturtle.pendown()
+        for i in range(self.clk.min()):
+            self.minturtle.forward(18.849555921538759430775860299677)
+            self.minturtle.right(6)
+
+    def writeHour(self):
+        self.hourturtle.clear()
+        self.hourturtle.reset()
+        self.hourturtle.penup()
+        self.hourturtle.setheading(90)
+        self.hourturtle.forward(160)
+        self.hourturtle.right(90 + 3)
+        self.hourturtle.pendown()
+        for i in range(self.clk.hour12() * 6):
+            self.hourturtle.forward(83.775804095727819692337156887453 / 6)
+            self.hourturtle.right(30 / 6)
 
     def __init__(self):
         self.secturtle._delay(0)
@@ -23,10 +51,15 @@ class Digitalclock:
         self.clk.setOnSecondChangeListener(self.writeSec)
         self.clk.setOnMinuteChangeListener(self.writeMin)
         self.clk.setOnHourChangeListener(self.writeHour)
-
-        self.Sectest()
-
+        self.printToConsole()
         self.scr.mainloop()
 
-    def Sectest(self):
+    def printToConsole(self):
+        print("Hour: " + str(self.clk.leftNumber(self.clk.hour24())) + "_" + str(
+            self.clk.rightNumber(self.clk.hour24())))
+        print(
+            "Minute: " + str(self.clk.leftNumber(self.clk.min())) + "_" + str(self.clk.rightNumber(self.clk.min())))
+        print(
+            "Second: " + str(self.clk.leftNumber(self.clk.sec())) + "_" + str(self.clk.rightNumber(self.clk.sec())))
 
+Kattintgato()

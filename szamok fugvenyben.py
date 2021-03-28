5cd0d9ce155e2afeafa011f27861459aa6cd23f6
Benceplays <nemeth.csaba.bence@gmail.com>
2021-03-28 20:15:28 +0200

    Szamok, felkesz

--- szamok fugvenyben.py
+++ szamok fugvenyben.py
@@ -54,11 +54,58 @@ class Szamok:
         self.t.penup()
         self.t.left(90)
         self.t.forward(20)
+        self.t.pendown()
+        self.t.right(135)
+        for g in range(230):
+            self.t.forward(1)
+            self.t.left(1)
+        self.t.forward(25)
+        self.t.right(95)
+        self.t.forward(100)
         self.t.right(90)
+        self.t.forward(100)
+
+    def hat(self):
+        for v in range(4):
+            self.t.forward(100)
+            self.t.right(90)
+        self.t.left(90)
+        self.t.forward(100)
+        self.t.right(90)
+        self.t.forward(100)
+
+    def het(self):
+        self.t.penup()
+        self.t.forward(100)
+        self.t.pendown()
+        self.t.left(90)
+        self.t.forward(200)
+        self.t.left(90)
+        self.t.forward(100)
+
+    def nyolc(self):
+        for e in range(4):
+            self.t.forward(100)
+            self.t.left(90)
+        self.t.left(90)
+        self.t.forward(100)
+        self.t.right(90)
+        for e in range(4):
+            self.t.forward(100)
+            self.t.left(90)
+
+    def kilenc(self):
+        self.t.forward(100)
+        self.t.left(90)
+        self.t.forward(100)
+        for z in range(4):
+            self.t.forward(100)
+            self.t.left(90)
+
 
     def __init__(self):
         self.t.speed(0)
-        self.negy()
+        self.kilenc()
 
         self.scr.mainloop()
 

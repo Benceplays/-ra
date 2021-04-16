63770d556ef556313d5590440b4e4e4fdf982158
Benceplays <nemeth.csaba.bence@gmail.com>
2021-04-16 14:05:21 +0200

    Feles

--- ora.py
+++ ora.py
@@ -270,6 +270,137 @@ class Kattintgato:
             self.minturtle.forward(18.849555921538759430775860299677)
             self.minturtle.right(6)
 
+    def pozicio2(self):
+        self.minturtle.penup()
+        self.minturtle.backward(500)
+        self.minturtle.pendown()
+
+
+    def kirajzolo3(self, x):
+        if self.clk.min() != x:
+            self.num2.clearer()
+        if self.clk.min() == x:
+            if x == 0:
+                self.num2.nulla()
+            if x == 1:
+                self.num2.egy()
+            if x == 2:
+                self.num2.ketto()
+            if x == 3:
+                self.num2.harom()
+            if x == 4:
+                self.num2.negy()
+            if x == 5:
+                self.num2.ot()
+            if x == 6:
+                self.num2.hat()
+            if x == 7:
+                self.num2.het()
+            if x == 8:
+                self.num2.nyolc()
+            if x == 9:
+                self.num2.kilenc()
+            if x == 10:
+                self.num2.nulla()
+            if x == 11:
+                self.num2.egy()
+            if x == 12:
+                self.num2.ketto()
+            if x == 13:
+                self.num2.harom()
+            if x == 14:
+                self.num2.negy()
+            if x == 15:
+                self.num2.ot()
+            if x == 16:
+                self.num2.hat()
+            if x == 17:
+                self.num2.het()
+            if x == 18:
+                self.num2.nyolc()
+            if x == 19:
+                self.num2.kilenc()
+            if x == 20:
+                self.num2.nulla()
+            if x == 21:
+                self.num2.egy()
+            if x == 22:
+                self.num2.ketto()
+            if x == 23:
+                self.num2.harom()
+            if x == 24:
+                self.num2.negy()
+            if x == 25:
+                self.num2.ot()
+            if x == 26:
+                self.num2.hat()
+            if x == 27:
+                self.num2.het()
+            if x == 28:
+                self.num2.nyolc()
+            if x == 29:
+                self.num2.kilenc()
+            if x == 30:
+                self.num2.nulla()
+            if x == 31:
+                self.num2.egy()
+            if x == 32:
+                self.num2.ketto()
+            if x == 33:
+                self.num2.harom()
+            if x == 34:
+                self.num2.negy()
+            if x == 35:
+                self.num2.ot()
+            if x == 36:
+                self.num2.hat()
+            if x == 37:
+                self.num2.het()
+            if x == 38:
+                self.num2.nyolc()
+            if x == 39:
+                self.num2.kilenc()
+            if x == 40:
+                self.num2.nulla()
+            if x == 41:
+                self.num2.egy()
+            if x == 42:
+                self.num2.ketto()
+            if x == 43:
+                self.num2.harom()
+            if x == 44:
+                self.num2.negy()
+            if x == 45:
+                self.num2.ot()
+            if x == 46:
+                self.num2.hat()
+            if x == 47:
+                self.num2.het()
+            if x == 48:
+                self.num2.nyolc()
+            if x == 49:
+                self.num2.kilenc()
+            if x == 50:
+                self.num2.nulla()
+            if x == 51:
+                self.num2.egy()
+            if x == 52:
+                self.num2.ketto()
+            if x == 53:
+                self.num2.harom()
+            if x == 54:
+                self.num2.negy()
+            if x == 55:
+                self.num2.ot()
+            if x == 56:
+                self.num2.hat()
+            if x == 57:
+                self.num2.het()
+            if x == 58:
+                self.num2.nyolc()
+            if x == 59:
+                self.num2.kilenc()
+
     def writeHour(self):
         self.hourturtle.clear()
         self.hourturtle.reset()
@@ -281,6 +412,139 @@ class Kattintgato:
         for i in range(self.clk.hour12() * 6):
             self.hourturtle.forward(83.775804095727819692337156887453 / 6)
             self.hourturtle.right(30 / 6)
+
+    def hour12(self):
+        self.kirajzolo4(1)
+
+    def pozicio3(self):
+        self.minturtle.penup()
+        self.minturtle.backward(500)
+        self.minturtle.pendown()
+
+    def kirajzolo4(self, x):
+        if self.clk.hour12() != x:
+            self.num.clearer()
+        if self.clk.hour12() == x:
+            if x == 0:
+                self.num4.nulla()
+            if x == 1:
+                self.num4.egy()
+            if x == 2:
+                self.num4.ketto()
+            if x == 3:
+                self.num4.harom()
+            if x == 4:
+                self.num4.negy()
+            if x == 5:
+                self.num4.ot()
+            if x == 6:
+                self.num4.hat()
+            if x == 7:
+                self.num4.het()
+            if x == 8:
+                self.num4.nyolc()
+            if x == 9:
+                self.num4.kilenc()
+            if x == 10:
+                self.num4.nulla()
+            if x == 11:
+                self.num4.egy()
+            if x == 12:
+                self.num4.ketto()
+            if x == 13:
+                self.num4.harom()
+            if x == 14:
+                self.num4.negy()
+            if x == 15:
+                self.num4.ot()
+            if x == 16:
+                self.num4.hat()
+            if x == 17:
+                self.num4.het()
+            if x == 18:
+                self.num4.nyolc()
+            if x == 19:
+                self.num4.kilenc()
+            if x == 20:
+                self.num4.nulla()
+            if x == 21:
+                self.num4.egy()
+            if x == 22:
+                self.num4.ketto()
+            if x == 23:
+                self.num4.harom()
+            if x == 24:
+                self.num4.negy()
+            if x == 25:
+                self.num4.ot()
+            if x == 26:
+                self.num4.hat()
+            if x == 27:
+                self.num4.het()
+            if x == 28:
+                self.num4.nyolc()
+            if x == 29:
+                self.num4.kilenc()
+            if x == 30:
+                self.num4.nulla()
+            if x == 31:
+                self.num4.egy()
+            if x == 32:
+                self.num4.ketto()
+            if x == 33:
+                self.num4.harom()
+            if x == 34:
+                self.num4.negy()
+            if x == 35:
+                self.num4.ot()
+            if x == 36:
+                self.num4.hat()
+            if x == 37:
+                self.num4.het()
+            if x == 38:
+                self.num4.nyolc()
+            if x == 39:
+                self.num4.kilenc()
+            if x == 40:
+                self.num4.nulla()
+            if x == 41:
+                self.num4.egy()
+            if x == 42:
+                self.num4.ketto()
+            if x == 43:
+                self.num4.harom()
+            if x == 44:
+                self.num4.negy()
+            if x == 45:
+                self.num4.ot()
+            if x == 46:
+                self.num4.hat()
+            if x == 47:
+                self.num4.het()
+            if x == 48:
+                self.num4.nyolc()
+            if x == 49:
+                self.num4.kilenc()
+            if x == 50:
+                self.num4.nulla()
+            if x == 51:
+                self.num4.egy()
+            if x == 52:
+                self.num4.ketto()
+            if x == 53:
+                self.num4.harom()
+            if x == 54:
+                self.num4.negy()
+            if x == 55:
+                self.num4.ot()
+            if x == 56:
+                self.num4.hat()
+            if x == 57:
+                self.num4.het()
+            if x == 58:
+                self.num4.nyolc()
+            if x == 59:
+                self.num4.kilenc()
 
     def __init__(self):
         self.scr.bgcolor('black')
